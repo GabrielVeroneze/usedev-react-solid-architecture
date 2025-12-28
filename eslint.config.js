70993b30ece68c8068b14a5b4c626fd4b13ca220
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-12-28 16:35:18 -0300

    🔧 chore: ajusta regra de hooks do React
    Substitui a desativação da regra `react-hooks/exhaustive-deps`
    por `react-hooks/set-state-in-effect`, alinhando o lint às
    decisões atuais do projeto sobre uso de efeitos e setState.

--- eslint.config.js
+++ eslint.config.js
@@ -20,7 +20,7 @@ export default defineConfig([
             globals: globals.browser,
         },
         rules: {
-            'react-hooks/exhaustive-deps': 'off',
+            'react-hooks/set-state-in-effect': 'off',
         },
     },
 ])
