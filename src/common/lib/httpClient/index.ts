5bace4b553923baf1baa912a128a3de7713f4591
Gabriel Veroneze <95183901+GabrielVeroneze@users.noreply.github.com>
2025-12-28 16:40:32 -0300

    ✨ feat: implementa cliente HTTP com Axios
    Cria a implementação Http baseada em Axios que segue
    o contrato da interface HttpClient, encapsulando a requisição GET e retornando apenas os dados da resposta.

---
+++ src/common/lib/httpClient/index.ts
@@ -0,0 +1,10 @@
+import type { HttpClient } from './HttpClient'
+import axios from 'axios'
+
+export const Http = (): HttpClient => {
+    return {
+        get: async <T>(url: string): Promise<T> => {
+            return axios.get<T>(url).then((response) => response.data)
+        },
+    }
+}
